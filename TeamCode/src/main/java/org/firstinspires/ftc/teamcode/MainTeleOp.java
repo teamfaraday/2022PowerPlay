c25e32d6fd934d0facf44f741534f9aa52ad315f
Devam Mondal <devammondal06@gmail.com>
2022-09-26 18:14:18 -0400

    init

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleOp.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name = "MainTeleOp")
+public class MainTeleOp extends LinearOpMode {
+
+    private DcMotor frontLeft;
+    private DcMotor frontRight;
+    private DcMotor
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorConfigHelperClass.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+public class MotorConfigHelperClass {
+
+    private DcMotor frontLeft;
+    private DcMotor frontRight;
+    private DcMotor backLeft;
+    private DcMotor backRight;
+
+    public MotorConfigHelperClass(){
+
+        frontLeft = hardwareMap.
+
+
+    }
+
+}
